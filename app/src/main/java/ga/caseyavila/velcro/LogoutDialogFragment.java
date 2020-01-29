b705ea9ade12e511d1b3cc27119c74c74049a77a
kc-kc <casey@theavilas.org>
2020-01-28 21:03:41 -0800

    Updated the logout Dialog theme

--- app/src/main/java/ga/caseyavila/velcro/LogoutDialogFragment.java
+++ app/src/main/java/ga/caseyavila/velcro/LogoutDialogFragment.java
@@ -18,7 +18,7 @@ public class LogoutDialogFragment extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        AlertDialog.Builder builder = new AlertDialog.Builder(Objects.requireNonNull(getActivity()), R.style.AlertDialogTheme);
         builder.setMessage(R.string.logout_dialog_message)
                 .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialogInterface, int i) {
