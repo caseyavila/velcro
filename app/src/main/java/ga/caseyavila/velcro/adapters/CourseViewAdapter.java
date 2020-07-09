9dff68c5f23056e31b17f05e66ba8ffb6a60d50d
Casey Avila <casey@theavilas.org>
2020-07-09 03:24:17 -0700

    Updated graph for actual use

--- app/src/main/java/ga/caseyavila/velcro/adapters/CourseViewAdapter.java
+++ app/src/main/java/ga/caseyavila/velcro/adapters/CourseViewAdapter.java
@@ -1,15 +1,20 @@
 package ga.caseyavila.velcro.adapters;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.material.textview.MaterialTextView;
 import ga.caseyavila.velcro.R;
-import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.*;
 import lecho.lib.hellocharts.view.LineChartView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static ga.caseyavila.velcro.activities.LoginActivity.casey;
 
@@ -41,8 +46,50 @@ public class CourseViewAdapter extends RecyclerView.Adapter<CourseViewAdapter.Vi
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         if (getItemViewType(position) == COURSE_VIEW_TYPES.HEADER) {
             holder.headerTeacher.setText(casey.getTeacher(period));
+            LineChartView lineChartView = holder.trendChart;
+
+            String[] xAxisData = {"Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"};
+            int[] yAxisData = {50, 20, 15, 30, 20, 60, 15, 40, 45, 10, 105, 18};
+
+            List yAxisValues = new ArrayList();
+            List axisValues = new ArrayList();
+
+
+            Line line = new Line(yAxisValues).setColor(ContextCompat.getColor(context, R.color.colorPrimary));
+            line.setFilled(true);
+            line.setHasPoints(false);
+
+            for (int i = 0; i < xAxisData.length; i++) {
+                axisValues.add(i, new AxisValue(i).setLabel(xAxisData[i]));
+            }
+
+            for (int i = 0; i < yAxisData.length; i++) {
+                yAxisValues.add(new PointValue(i, yAxisData[i]));
+            }
+
+            List lines = new ArrayList();
+            lines.add(line);
 
             LineChartData data = new LineChartData();
+            data.setLines(lines);
+
+            Axis axis = new Axis();
+            axis.setValues(axisValues);
+            axis.setTextSize(14);
+            axis.setHasLines(true);
+            axis.setTextColor(ContextCompat.getColor(context, R.color.textHighEmphasis));
+            axis.setLineColor(ContextCompat.getColor(context, R.color.textMediumEmphasis));
+            data.setAxisXBottom(axis);
+
+            Axis yAxis = new Axis(Axis.generateAxisFromRange(0, 110, 25));
+            yAxis.setHasLines(true);
+            yAxis.setTextColor(ContextCompat.getColor(context, R.color.textHighEmphasis));
+            yAxis.setLineColor(ContextCompat.getColor(context, R.color.textMediumEmphasis));
+            yAxis.setTextSize(14);
+            data.setAxisYLeft(yAxis);
+
+            lineChartView.setLineChartData(data);
+            lineChartView.setInteractive(false);
 
         } else {
             //Subtract one due to the offset the header card creates
